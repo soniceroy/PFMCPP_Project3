fbb0b75cae0f464b29fc276b26c60597cd9201b6
matkatmusic <matkatmusic@gmail.com>
2021-09-06 11:21:34 -0700

    Added part 1b

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 /*
-Project 3 - Part 1a / 5
+Project 3 - Part 1b / 5
 Video:  Chapter 2 Part 5
 User-Defined Types
 
-Create a branch named Part1
+Continue your work on branch Part1
 
 Purpose: The entire purpose of this 5-part project is to get you writing C++ code that compiles and to 
 reinforce the syntax habits that C++ requires.  
@@ -13,7 +13,7 @@ What you create in this project will be used as the basis of Project 5 in the co
 Part1 purpose:  Learn to write UDTs
 
 You are going to write 10 UDTs in project3.  
-Part1 will be broken up into 5 separate steps, all on the same branch
+Part1 will be broken up into 5 separate steps
     Part 1a: you will learn to think about an object in terms of its sub-objects.
     Part 1b: you will write 4 un-related UDTs in plain english
     Part 1c: you will write 1 UDT in plain english that will be made of 5 related sub-objects
@@ -21,67 +21,99 @@ Part1 will be broken up into 5 separate steps, all on the same branch
     Part 1e: you will convert those 10 plain-english UDTs into code that runs.
 ************************
 
-1) Look at the picture of the car interior (Part1a pic.jpg).  
-    Fill in the blanks below which break this car interior down into sub-objects.
-
-    Several sub-objects are listed below that make up this car's interior.
-        you're going to name several things that you'll find on each subobject
-        you're going to name several things that each subobject can do.
-        If you've seen "Family Feud", we are going to do what they do in that show
-
-        A few blanks are filled in for you already.
-
-Main Object: Car Interior
-Sub Object 1: Steering Wheel
-    Name 4 things you'll find on the:    Steering Wheel
-        1) paddle shifters
-        2) 'cruise control' controls
-        3) horn button
-        4) the wheel itself
-    Name 2 things you can do with the:   Steering Wheel
-        1) adjust cruise control settings.
-        2) sound the horn.
-        
-Sub Object 2: Instrument Cluster
-    Name 4 things you'll find on the:   Instrument Cluster
-        1) speed gauge
-        2) rpm gauge
-        3) clock
-        4) temperature
-    Name 3 things you can do with the:   Instrument Cluster
-        1) get speed.
-        2) get rpm.
-        3) get time.
-    
-Sub Object 3: Environment Controls
-    Name 3 things you'll find on the:    Environment Controls
-        1) driver air condition controls
-        2) passenger air condition controls
-        3) rear defrost button
-    Name 3 things you can do with the:   Environment Controls
-        1) turn up/down the temperature for the driver.
-        2) turn up/down the temperature for the passenger.
-        3) turn on the rear defrost.
-
-Sub Object 4: Infotainment System
-    Name 3 things you'll find on the:    Infotainment System
-        1) radio station/ track selection
-        2) search button
-        3) pause/play button
-    Name 3 things you can do with the:   Infotainment System
-        1) pick a track or radio station.
-        2) search for a track or radio station.
-        3) pause/play selected track or radio station.
-
-Sub Object 5: Seat 
-    Name 3 things you'll find on the:    Seat
-        1) up/down seat button
-        2) up/down back rest button
-        3) seat forward/back putton
-    Name 2 things you can do with the:   Seat
-        1) raise/lower the seat
-        2) move the seat forward and back
+1)  Fill in #1 - 4 with a random UDT in plain english
+    These 4 UDTs do not need to be related.
+        a) For each plain-english UDT, write out 5 traits or properties and 3 things it can do
+        b) remember: these properties will eventually become primitives.
+        pick properties that can be represented with 'int float double bool char std::string'
 */
+
+/*
+ example:  
+ 
+Thing: Car Wash   
+    5 properties:
+        - number of vacuum cleaners
+        - number of eco-friendly cleaning supplies
+        - the amount of water used per week.
+        - amount of profit made per week
+        - number of cars serviced per day
+    3 things it can do:
+        - wash and wax car
+        - charge customer
+        - detail the car interior
+
+    Notice that I did not use "has a vacuum cleaner" or "Has eco-friendly cleaning supplies" as one of the properties
+    
+    Writing 'has a ___" checks whether or not your object **has the ability to do something**.
+    Instead, I wrote "number of vacuum cleaners" and "number of eco-friendly cleaning supplies".  
+    
+    These are specific objects or amounts. 
+    
+    In C++ terms, this means to I want you to avoid using 'bool' (has a) as a member variable type.
+    Instead, prefer the other primitive types.
+
+    In addition to the usual primitives (covered in Project 2), you can use 'std::string' to represent strings in this project.
+ */
+
+/*
+Thing 1)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
+ */
+
+/*
+Thing 2)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
+ */
+
+/*
+Thing 3)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
+ */
+
+/*
+Thing 4)
+5 properties:
+    1)
+    2)
+    3)
+    4)
+    5)
+3 things it can do:
+    1)
+    2)
+    3)
+ */
+
+
+
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
